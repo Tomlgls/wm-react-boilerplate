a861ded08ed57c75f310e1cbe2657b1dc1dd5bf9
tomlgls <tom.langlais.pro@gmail.com>
2018-12-13 17:22:33 +0100

    Improve page styling

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import {
   BrowserRouter as Router,
   Route,
   Redirect,
+  NavLink,
   Link,
   Switch
 } from 'react-router-dom';
 
 import Home from './views/Home';
 
+import config from './config';
+
 import'./app.css';
 
 const renderExView = () => {
-  return <div className="Ex-view">I am a view.</div>
+  return (
+      <div className="Ex-view">
+        <h2>Hello World!</h2>
+        <p>I am a view.</p>
+      </div>
+  )
 }
 
 const render404 = () => {
-  return <h1>404 Page.</h1>
+  return <h1 style={{textAlign: "center"}}>404 Page.</h1>
 }
 
 class App extends Component {
@@ -25,42 +33,37 @@ class App extends Component {
       <div className="App">
         <Router>
           <div className="react-root-class">
-
             <header className="Header">
-              <h1>React redux WM boilerplate</h1>
+              <h1>WM React boilerplate</h1>
+              <nav role="navigation" className="Nav">
+                <ul>
+                  <li><Link to="/">Home</Link></li>
+                  <li><NavLink to="/ex-view">Example of view</NavLink></li>
+                  <li><NavLink to="/azdazdazdazddaz">404</NavLink></li>
+                </ul>
+              </nav>
             </header>
-
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/ex-view">Ex view</Link></li>
-              <li><Link to="/azdazdazdazddaz">404</Link></li>
-            </ul>
-
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/ex-view" component={renderExView} />
-
               {
                 /**
-                * Pass props to a route
+                * How pass props to a route
                 *
                   <Route
                     path="/shop"
-                    render={(props) => ( <Shop {...props} userStatus={isUserLoggedIn} cartStatus={isCartProducts} shopReady={shopReady} /> )}
+                    render={(props) => ( <Shop {...props} aProps={propsExemple} /> )}
                   />
 
                 */
               }
-
-
               <Route path="*" component={render404} />
               <Redirect from="*" to="/notFound" />
             </Switch>
-
             <footer className="Footer">
-              Wall-Market
+              <p>Currently {config.site_version}. Released under the MIT License.</p>
+              <p>&copy; {new Date().getFullYear()} Wall-Market.</p>
             </footer>
-
           </div>
         </Router>
       </div>
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = () => ({
+  site_version: "@beta",
+  dist_version: "@beta",
+  site_name: "WM - Components"
+})
+
+export default new config();
